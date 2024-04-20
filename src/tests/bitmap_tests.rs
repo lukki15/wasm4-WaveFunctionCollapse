0ead0528fa46962b5b2f82cf2b2eb6ce4019b46b
lukki15 <50550773+lukki15@users.noreply.github.com>
2024-04-20 10:25:36 +0000

    fix bitmap tests

--- src/tests/bitmap_tests.rs
+++ src/tests/bitmap_tests.rs
@@ -34,27 +34,3 @@ fn first_pixels() {
         }
     }
 }
-
-#[test]
-fn first_pixels() {
-    const BITMAP_MASK: u8 = if PATTERN_N >= 4 {
-        0xFF
-    } else {
-        0xFF << (2 * (4 - PATTERN_N))
-    };
-
-    for x in 0..PATTERN_X {
-        for y in 0..PATTERN_Y {
-            let first_section = get_pattern(x, y);
-            let first_section_byte_pixel = first_section[0] & BITMAP_MASK;
-
-            let bitmap_pixel = (y * PATTERN_N) * BITMAP_WIDTH + x * PATTERN_N;
-            let bitmap_index = bitmap_pixel / 4;
-            let bitmap_offset = bitmap_pixel % 4;
-            let first_bitmap_byte_pixel =
-                (BITMAP[bitmap_index] << (2 * bitmap_offset)) & BITMAP_MASK;
-
-            assert_eq!(first_section_byte_pixel, first_bitmap_byte_pixel);
-        }
-    }
-}
