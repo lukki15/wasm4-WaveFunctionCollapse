a8bf49d6a282c66a9a8d4374a3074957d70c3811
lukki15 <50550773+lukki15@users.noreply.github.com>
2024-04-21 11:32:31 +0000

    add tests to verify every singel pixel value
    only works for BLIT_2BPP

--- src/bitmap.rs
+++ src/bitmap.rs
@@ -10,8 +10,8 @@ pub const BITMAP: [u8; BITMAP_WIDTH * BITMAP_HEIGHT / 4] = [
     0x55, 0x57, 0x55, 0x55, 0x55, 0x57, 0x55, 0x55, 0xaa, 0xab, 0xaa, 0xaa, 0xaa, 0xaa, 0xaa, 0xaa,
 ];
 
-const BITMAP_BIT_PER_BIXEL: usize = if BITMAP_FLAGS == 0 { 1 } else { 2 };
-const BITMAP_PIXELS_PER_BYTE: usize = 8 / BITMAP_BIT_PER_BIXEL;
+pub const BITMAP_BIT_PER_PIXEL: usize = if BITMAP_FLAGS == 0 { 1 } else { 2 };
+pub const BITMAP_PIXELS_PER_BYTE: usize = 8 / BITMAP_BIT_PER_PIXEL;
 
 pub const PATTERN_N: usize = 2;
 pub const PATTERN_X: usize = BITMAP_WIDTH / PATTERN_N;
@@ -25,7 +25,7 @@ fn get_pixels(x: usize, y: usize) -> u8 {
     let byte_index = pixel / BITMAP_PIXELS_PER_BYTE;
     let byte_offset = pixel % BITMAP_PIXELS_PER_BYTE;
 
-    BITMAP[byte_index] << (BITMAP_BIT_PER_BIXEL * byte_offset)
+    BITMAP[byte_index] << (BITMAP_BIT_PER_PIXEL * byte_offset)
 }
 
 const PATTERN_ARRAY_SIZE: usize =
@@ -45,7 +45,7 @@ pub fn _get_pattern2(x: usize, y: usize) -> PatternArray {
             let mask_pixel = if PATTERN_N > BITMAP_PIXELS_PER_BYTE {
                 0xFF
             } else {
-                const SHIFT: usize = BITMAP_BIT_PER_BIXEL * (BITMAP_PIXELS_PER_BYTE - PATTERN_N);
+                const SHIFT: usize = BITMAP_BIT_PER_PIXEL * (BITMAP_PIXELS_PER_BYTE - PATTERN_N);
                 0xFF << SHIFT
             };
 
--- src/tests/bitmap_tests.rs
+++ src/tests/bitmap_tests.rs
@@ -34,3 +34,37 @@ fn first_pixels() {
         }
     }
 }
+
+fn get_bitmap_pixel(x: usize, y: usize) -> u8 {
+    let bitmap_index = y * BITMAP_WIDTH + x;
+    let byte_index = bitmap_index / BITMAP_PIXELS_PER_BYTE;
+    let byte_offset = BITMAP_PIXELS_PER_BYTE - 1 - (bitmap_index % BITMAP_PIXELS_PER_BYTE);
+
+    BITMAP[byte_index] >> (BITMAP_BIT_PER_PIXEL * byte_offset) & 0x03
+}
+
+fn get_pattern_pixel(x: usize, y: usize) -> u8 {
+    let pattern_y = y / PATTERN_N;
+    let pattern_y_offset = y % PATTERN_N;
+
+    let pattern_x = x / PATTERN_N;
+    let pattern_x_offset = x % PATTERN_N;
+
+    let pattern_pixels = get_pattern(pattern_x, pattern_y);
+    let pattern_index = pattern_y_offset * PATTERN_N + pattern_x_offset;
+    let byte_index = pattern_index / BITMAP_PIXELS_PER_BYTE;
+    let byte_offset = BITMAP_PIXELS_PER_BYTE - 1 - (pattern_index % BITMAP_PIXELS_PER_BYTE);
+
+    pattern_pixels[byte_index] >> (BITMAP_BIT_PER_PIXEL * byte_offset) & 0x03
+}
+
+#[test]
+fn pixel_by_piyel() {
+    for y in 0..BITMAP_HEIGHT {
+        for x in 0..BITMAP_WIDTH {
+            let bitmap_pixel = get_bitmap_pixel(x, y);
+            let pattern_pixel = get_pattern_pixel(x,y);
+            assert_eq!(pattern_pixel, bitmap_pixel);
+        }
+    }
+}
