4d0d437126c1a567e354c0b95c0dc427b5c71319
lukki15 <50550773+lukki15@users.noreply.github.com>
2024-04-24 18:51:25 +0000

    use the original 15x24 image
    remove unstable test

--- src/tests/bitmap_tests.rs
+++ src/tests/bitmap_tests.rs
@@ -11,30 +11,6 @@ fn first_pixel() {
     assert_eq!(first_section_pixel, first_bitmap_pixel);
 }
 
-#[test]
-fn first_pixels() {
-    const BITMAP_MASK: u8 = if PATTERN_N >= 4 {
-        0xFF
-    } else {
-        0xFF << (2 * (4 - PATTERN_N))
-    };
-
-    for x in 0..PATTERN_X {
-        for y in 0..PATTERN_Y {
-            let first_section = get_pattern(x, y);
-            let first_section_byte_pixel = first_section[0] & BITMAP_MASK;
-
-            let bitmap_pixel = (y * PATTERN_N) * BITMAP_WIDTH + x * PATTERN_N;
-            let bitmap_index = bitmap_pixel / 4;
-            let bitmap_offset = bitmap_pixel % 4;
-            let first_bitmap_byte_pixel =
-                (BITMAP[bitmap_index] << (2 * bitmap_offset)) & BITMAP_MASK;
-
-            assert_eq!(first_section_byte_pixel, first_bitmap_byte_pixel);
-        }
-    }
-}
-
 fn get_bitmap_pixel(x: usize, y: usize) -> u8 {
     let bitmap_index = y * BITMAP_WIDTH + x;
     let byte_index = bitmap_index / BITMAP_PIXELS_PER_BYTE;
