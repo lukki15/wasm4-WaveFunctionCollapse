b86226f2432c4ce293c68a2ae05a4b25d8df3dae
lukki15 <50550773+lukki15@users.noreply.github.com>
2024-05-04 06:29:46 +0000

    add generator that manages the patterns and grid

---
+++ src/generator.rs
@@ -0,0 +1,69 @@
+use crate::constants::*;
+use crate::matcher;
+use crate::pattern;
+use crate::wasm4;
+use std::collections::BTreeSet;
+
+const PATTERN_SIZE: usize = wasm4::SCREEN_SIZE / PATTERN_N;
+
+pub struct Generator {
+    patterns: Vec<pattern::Pattern>,
+    grid: [[BTreeSet<usize>; PATTERN_SIZE]; PATTERN_SIZE],
+}
+
+impl Generator {
+    pub fn new() -> Self {
+        let patterns = matcher::generate_patterns();
+
+        const ARRAY_REPEAT_VALUE: BTreeSet<usize> = BTreeSet::new();
+        const ROW_REPEAT_VALUE: [BTreeSet<usize>; PATTERN_SIZE] =
+            [ARRAY_REPEAT_VALUE; PATTERN_SIZE];
+        let mut grid = [ROW_REPEAT_VALUE; PATTERN_SIZE];
+
+        for x in 0..PATTERN_SIZE {
+            for y in 0..PATTERN_SIZE {
+                grid[x][y] = (0..patterns.len()).collect();
+            }
+        }
+
+        Generator { patterns, grid }
+    }
+
+    pub fn init_borders(&mut self) {
+        for pattern_index in 0..self.patterns.len() {
+            let pattern = &self.patterns[pattern_index];
+            for x in 0..PATTERN_SIZE {
+                {
+                    // top
+                    const Y: usize = 0;
+                    if !pattern.get_top_border() {
+                        self.grid[x][Y].remove(&pattern_index);
+                    }
+                }
+                {
+                    // bottom
+                    const Y: usize = PATTERN_SIZE - 1;
+                    if !pattern.get_bottom_border() {
+                        self.grid[x][Y].remove(&pattern_index);
+                    }
+                }
+            }
+            for y in 0..PATTERN_SIZE {
+                {
+                    // left
+                    const X: usize = 0;
+                    if !pattern.get_left_border() {
+                        self.grid[X][y].remove(&pattern_index);
+                    }
+                }
+                {
+                    // right
+                    const X: usize = PATTERN_SIZE - 1;
+                    if !pattern.get_right_border() {
+                        self.grid[X][y].remove(&pattern_index);
+                    }
+                }
+            }
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod tests;
 mod alloc;
 mod bitmap;
 mod constants;
+mod generator;
 mod matcher;
 mod palette;
 mod pattern;
@@ -15,13 +16,15 @@ use lazy_static::lazy_static;
 use std::sync::Mutex;
 
 lazy_static! {
-    static ref PATTERNS: Mutex<Vec<pattern::Pattern>> = Mutex::new(matcher::generate_patterns());
+    static ref GENERATOR: Mutex<generator::Generator> = Mutex::new(generator::Generator::new());
 }
 
 #[no_mangle]
 fn start() {
     palette::set_palette([0xbfe8f2, 0xb97a57, 0xfff200, 0x00aa00]);
     palette::set_draw_colour(0x4203);
+
+    GENERATOR.lock().expect("generator").init_borders();
 }
 
 #[no_mangle]
